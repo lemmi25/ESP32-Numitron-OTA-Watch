43ff16671cf52fe79ed88129e2eb548c2a2b8be3
Moritz Boesenberg <moritz@signal2x.com>
2019-09-19 16:57:22 +0200

    added files description will follow

---
+++ lib/TLC5916/TLC591x.cpp
@@ -0,0 +1,243 @@
+/* Library to interface with the Texas Instruments TLC5916 and TLC5917
+   8-Channel Constant-Current LED Sink Drivers.
+   https://gitlab.com/Andy4495/TLC591x
+
+*/
+/* Version History
+   1.0.0    08/03/2018  A.T.   Original
+*/
+
+#include "TLC591x.h"
+
+TLC591x::TLC591x(byte n, byte SDI, byte CLK, byte LE, byte OE)
+{
+  SDI_pin = SDI;
+  CLK_pin = CLK;
+  LE_pin = LE;
+  OE_pin = OE;
+  numchips = n;
+
+  digitalWrite(OE_pin, HIGH);
+  pinMode(OE_pin, OUTPUT);
+  init();
+}
+
+TLC591x::TLC591x(byte n, byte SDI, byte CLK, byte LE)
+{
+  SDI_pin = SDI;
+  CLK_pin = CLK;
+  LE_pin = LE;
+  OE_pin = NO_PIN;
+  numchips = n;
+
+  init();
+}
+
+void TLC591x::init()
+{
+  if (numchips < MINCHIPS)
+    numchips = MINCHIPS;
+  if (numchips > MAXCHIPS)
+    numchips = MAXCHIPS;
+
+  pinMode(SDI_pin, OUTPUT);
+  pinMode(CLK_pin, OUTPUT);
+  digitalWrite(LE_pin, LOW);
+  pinMode(LE_pin, OUTPUT);
+}
+
+uint16_t TLC591x::convertFrom8To16(uint8_t dataFirst, uint8_t dataSecond)
+{
+  uint16_t dataBoth = 0x0000;
+
+  dataBoth = dataFirst;
+  dataBoth = dataBoth << 8;
+  dataBoth |= dataSecond;
+  return dataBoth;
+}
+
+uint8_t *TLC591x::convertFrom16To8(uint16_t dataAll)
+{
+  static uint8_t arrayData[2] = {0x00, 0x00};
+
+  *(arrayData) = (dataAll >> 8) & 0x00FF;
+  arrayData[1] = dataAll & 0x00FF;
+  return arrayData;
+}
+
+void TLC591x::print(unsigned int number)
+{
+  printValue(convertFrom8To16(segements[number % 10], segements[(number / 10) % 10]));
+}
+
+void TLC591x::printValue(unsigned int n)
+{
+  for (byte i = 0; i < numchips; i++)
+  {
+    write(n);
+    n = n / 256; // Get the next most significant byte
+  }
+  toggleLE();
+}
+
+void TLC591x::write(byte n)
+{
+  digitalWrite(SDI_pin, n & 0x01);
+  toggleCLK();
+  digitalWrite(SDI_pin, n & 0x02);
+  toggleCLK();
+  digitalWrite(SDI_pin, n & 0x04);
+  toggleCLK();
+  digitalWrite(SDI_pin, n & 0x08);
+  toggleCLK();
+  digitalWrite(SDI_pin, n & 0x10);
+  toggleCLK();
+  digitalWrite(SDI_pin, n & 0x20);
+  toggleCLK();
+  digitalWrite(SDI_pin, n & 0x40);
+  toggleCLK();
+  digitalWrite(SDI_pin, n & 0x80);
+  toggleCLK();
+}
+
+void TLC591x::toggleLE()
+{
+  // TLC5916 minimum LE pulse time is 20 ns, so don't need to worry about
+  // putting in a hard-coded delay.
+  digitalWrite(LE_pin, HIGH);
+  digitalWrite(LE_pin, LOW);
+}
+
+void TLC591x::toggleCLK()
+{
+  // TLC5916 minimum CLK pulse time is 20 ns, so don't need to worry about
+  // putting in a hard-coded delay.
+  digitalWrite(CLK_pin, HIGH);
+  digitalWrite(CLK_pin, LOW);
+}
+/* Library to interface with the Texas Instruments TLC5916 and TLC5917
+   8-Channel Constant-Current LED Sink Drivers.
+   https://gitlab.com/Andy4495/TLC591x
+*/
+/* Version History
+   1.0.0    08/03/2018  A.T.   Original
+*/
+
+/* #include "TLC591x.h"
+
+TLC591x::TLC591x(byte n, byte SDI, byte CLK, byte LE, byte OE)
+{
+  SDI_pin = SDI;
+  CLK_pin = CLK;
+  LE_pin = LE;
+  OE_pin = OE;
+  numchips = n;
+
+  digitalWrite(OE_pin, HIGH);
+  pinMode(OE_pin, OUTPUT);
+  init();
+}
+
+TLC591x::TLC591x(byte n, byte SDI, byte CLK, byte LE)
+{
+  SDI_pin = SDI;
+  CLK_pin = CLK;
+  LE_pin = LE;
+  OE_pin = NO_PIN;
+  numchips = n;
+
+  init();
+}
+
+void TLC591x::init()
+{
+  if (numchips < MINCHIPS)
+    numchips = MINCHIPS;
+  if (numchips > MAXCHIPS)
+    numchips = MAXCHIPS;
+
+  pinMode(SDI_pin, OUTPUT);
+  pinMode(CLK_pin, OUTPUT);
+  digitalWrite(LE_pin, LOW);
+  pinMode(LE_pin, OUTPUT);
+}
+
+void TLC591x::print(const char *s)
+{
+  int i;
+  char c;
+  byte pos;
+
+  for (i = numchips; i >= 0; i--)
+  {
+    // Need a range check and adjustment, since segment map starts
+    // at ASCII 32 (element 0) and ends at ASCII 0x7f (element 96)
+    // Out of range default to a blank character
+    // if (s[i] > 127) pos = 0; --> This would be a negative value, included in next check
+    if (s[i] < 32)
+      pos = 0;
+    else
+      pos = s[i] - 32;
+    c = ICM7218_segment_map[pos];
+    write(c);
+  }
+  toggleLE();
+}
+
+void TLC591x::print(unsigned int n)
+{
+  for (byte i = 0; i < numchips; i++)
+  {
+    write(n);
+    n = n / 256; // Get the next most significant byte
+  }
+  toggleLE();
+}
+
+void TLC591x::displayEnable()
+{
+  if (OE_pin != NO_PIN)
+    digitalWrite(OE_pin, LOW);
+}
+
+void TLC591x::displayDisable()
+{
+  if (OE_pin != NO_PIN)
+    digitalWrite(OE_pin, HIGH);
+}
+
+void TLC591x::write(byte n)
+{
+  digitalWrite(SDI_pin, n & 0x01);
+  toggleCLK();
+  digitalWrite(SDI_pin, n & 0x02);
+  toggleCLK();
+  digitalWrite(SDI_pin, n & 0x04);
+  toggleCLK();
+  digitalWrite(SDI_pin, n & 0x08);
+  toggleCLK();
+  digitalWrite(SDI_pin, n & 0x10);
+  toggleCLK();
+  digitalWrite(SDI_pin, n & 0x20);
+  toggleCLK();
+  digitalWrite(SDI_pin, n & 0x40);
+  toggleCLK();
+  digitalWrite(SDI_pin, n & 0x80);
+  toggleCLK();
+}
+
+void TLC591x::toggleLE()
+{
+  // TLC5916 minimum LE pulse time is 20 ns, so don't need to worry about
+  // putting in a hard-coded delay.
+  digitalWrite(LE_pin, HIGH);
+  digitalWrite(LE_pin, LOW);
+}
+
+void TLC591x::toggleCLK()
+{
+  // TLC5916 minimum CLK pulse time is 20 ns, so don't need to worry about
+  // putting in a hard-coded delay.
+  digitalWrite(CLK_pin, HIGH);
+  digitalWrite(CLK_pin, LOW);
+} */
---
+++ src/main.cpp
@@ -0,0 +1,167 @@
+/* Example sketch using TLC591x library  to interface
+   with the Texas Instruments TLC5916 and TLC5917
+   8-Channel Constant-Current LED Sink Drivers.
+   https://gitlab.com/Andy4495/TLC591x
+
+*/
+/* Version History
+   1.0.0    08/06/2018  A.T.   Original
+*/
+#include <TLC591x.h>
+#include <WiFi.h>
+#include <ESPmDNS.h>
+#include <WiFiUdp.h>
+#include <NTPClient.h>
+#include <ArduinoOTA.h>
+
+TLC591x seg1(2, 13, 26, 25); // Tube 1,2 SDI,CLK,LE
+TLC591x seg2(2, 4, 2, 5);    // Tube 3,4 SDI,CLK,LE
+
+// Define NTP Client to get time
+WiFiUDP ntpUDP;
+NTPClient timeClient(ntpUDP);
+
+// Variables to save date and time
+String formattedDate;
+String dateStamp;
+String timeStamp;
+int houre;
+int minute;
+int month;
+int day;
+
+const char *ssid = "KabelBox-FD5C";
+const char *password = "78966844438381424755";
+
+const byte interruptPin = 15;
+volatile bool interrupt = false;
+int numberOfInterrupts = 0;
+
+portMUX_TYPE mux = portMUX_INITIALIZER_UNLOCKED;
+
+volatile unsigned long alteZeit = 0, entprellZeit = 1000;
+
+void IRAM_ATTR handleInterrupt()
+{
+
+  if ((millis() - alteZeit) > entprellZeit)
+  {
+    portENTER_CRITICAL_ISR(&mux);
+    alteZeit = millis();
+    interrupt = true;
+    Serial.println(interrupt);
+    portEXIT_CRITICAL_ISR(&mux);
+  }
+}
+
+void setup()
+{
+  pinMode(interruptPin, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(interruptPin), handleInterrupt, FALLING);
+
+  Serial.begin(115200);
+  Serial.println("Booting");
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  while (WiFi.waitForConnectResult() != WL_CONNECTED)
+  {
+    Serial.println("Connection Failed! Rebooting...");
+    delay(5000);
+    ESP.restart();
+  }
+  // Hostname defaults to esp3232-[MAC]
+  ArduinoOTA.setHostname("OTA ESP32");
+
+  // No authentication by default
+  // ArduinoOTA.setPassword("admin");
+
+  // Password can be set with it's md5 value as well
+  // MD5(admin) = 21232f297a57a5a743894a0e4a801fc3
+  ArduinoOTA.setPasswordHash("21232f297a57a5a743894a0e4a801fc3");
+
+  ArduinoOTA.onStart([]() {
+    String type;
+    if (ArduinoOTA.getCommand() == U_FLASH)
+      type = "sketch";
+    else // U_SPIFFS
+      type = "filesystem";
+
+    Serial.println("Start updating " + type);
+  });
+  ArduinoOTA.onEnd([]() {
+    Serial.println("\nEnd");
+  });
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+  });
+  ArduinoOTA.onError([](ota_error_t error) {
+    Serial.printf("Error[%u]: ", error);
+    if (error == OTA_AUTH_ERROR)
+      Serial.println("Auth Failed");
+    else if (error == OTA_BEGIN_ERROR)
+      Serial.println("Begin Failed");
+    else if (error == OTA_CONNECT_ERROR)
+      Serial.println("Connect Failed");
+    else if (error == OTA_RECEIVE_ERROR)
+      Serial.println("Receive Failed");
+    else if (error == OTA_END_ERROR)
+      Serial.println("End Failed");
+  });
+  ArduinoOTA.begin();
+  Serial.println("Ready");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  // Initialize a NTPClient to get time
+  timeClient.begin();
+  // Set offset time in seconds to adjust for your timezone, for example:
+  // GMT +1 = 3600
+  // GMT +8 = 28800
+  // GMT -1 = -3600
+  // GMT 0 = 0
+  timeClient.setTimeOffset(7200);
+}
+
+void loop()
+{
+  ArduinoOTA.handle();
+  while (!timeClient.update())
+  {
+    timeClient.forceUpdate();
+  }
+
+  // Extract date
+  formattedDate = timeClient.getFormattedDate();
+  int splitT = formattedDate.indexOf("T");
+  dateStamp = formattedDate.substring(0, splitT - 8);
+
+  //Serial.println(interrupt);
+
+  if (interrupt == true)
+  {
+    dateStamp = formattedDate.substring(5, splitT - 3);
+    month = dateStamp.toInt();
+
+    seg2.print(month);
+
+    dateStamp = formattedDate.substring(8, splitT);
+    day = dateStamp.toInt();
+
+    seg1.print(day);
+
+    delay(3000);
+
+    interrupt = false;
+  }
+
+  // Extract time
+  timeStamp = formattedDate.substring(splitT + 1, formattedDate.length() - 7);
+  houre = timeStamp.toInt();
+
+  seg1.print(houre);
+
+  timeStamp = formattedDate.substring(splitT + 4, formattedDate.length() - 4);
+  minute = timeStamp.toInt();
+
+  seg2.print(minute);
+}
